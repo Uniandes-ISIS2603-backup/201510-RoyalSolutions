f2831bf53581849ecb6255ac8be1abb14a2c7bfc
David Bonilla <dc.bonilla10@uniandes.edu.co>
2015-03-19 11:04:34 -0500

    Refactorizacion de codigo

---
+++ Backend/Royal.logic/src/main/java/api/CalificacionLogic.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api;
+
+/**
+ *
+ * @author dc.bonilla10 y solo rojo loka.
+ */
+public interface CalificacionLogic {
+    
+    public void postCalificacion(  ); 
+    
+    
+}

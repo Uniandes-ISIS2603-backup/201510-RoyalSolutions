d2e73d9b317a4efd9762be51e78458b06039c097
David Bonilla <dc.bonilla10@uniandes.edu.co>
2015-03-26 22:39:50 -0500

    EJB Backend

--- Backend/Royal.logic/src/main/java/ejb/CalificacionLogic.java
+++ Backend/Royal.logic/src/main/java/ejb/CalificacionLogic.java
@@ -5,11 +5,46 @@
  */
 package ejb;
 
+import converter.CalificacionConverter;
+import dto.CalificacionDTO;
+import entity.CalificacionEntity;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
 /**
  *
  * @author estudiante
  */
 public class CalificacionLogic 
 {
-    
+    @PersistenceContext(unitName = "CalificacionClassPU")
+	    protected EntityManager entityManager;
+
+	    public CalificacionDTO createCalificacion(CalificacionDTO calif) {
+	        CalificacionEntity entity = CalificacionConverter.persistenceDTO2Entity(calif);
+	        entityManager.persist(entity);
+	        return CalificacionConverter.entity2PersistenceDTO(entity);
+	    }
+	    
+	    public List<CalificacionDTO> getCalificacions() {
+	        Query q = entityManager.createQuery("select u from CalificacionEntity u");
+	        return CalificacionConverter.entity2PersistenceDTOList(q.getResultList());
+	    }
+	    
+	     public CalificacionDTO getCalificacion(String name) {
+		        return CalificacionConverter.entity2PersistenceDTO(entityManager.find(CalificacionEntity.class, name));
+		    }
+
+		    public void deleteCalificacion(String name) {
+		        CalificacionEntity entity = entityManager.find(CalificacionEntity.class, name);
+		        entityManager.remove(entity);
+		    }
+
+		    public void updateCalificacion(CalificacionDTO Calificacion) {
+		        CalificacionEntity entity = entityManager.merge(CalificacionConverter.persistenceDTO2Entity(Calificacion));
+		        CalificacionConverter.entity2PersistenceDTO(entity);
+		    }    
 }

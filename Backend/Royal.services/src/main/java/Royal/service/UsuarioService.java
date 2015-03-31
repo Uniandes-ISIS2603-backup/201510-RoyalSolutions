90e4d94e0c13f7a67f6b29f688087de642135a2a
uniandes-jasilva11 <ja.silva11@uniandes.edu.co>
2015-03-31 12:30:50 -0500

    cambio pom

---
+++ Backend/Royal.services/src/main/java/Royal/service/UsuarioService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Royal.service;
+
+import Royal.usuario.logic.api.IUsuario;
+import Royal.usuario.logic.dto.UsuarioDTO;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author estudiante
+ */
+
+@Path("/usuario")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class UsuarioService
+{
+   @Inject
+    protected IUsuario LogicService;
+
+    @POST
+    public UsuarioDTO createUsuario(UsuarioDTO usuario) {
+        return LogicService.createUsuario(usuario);
+    }
+
+    @GET
+    @Path("{nombre}")
+    public UsuarioDTO getItineario(@PathParam("nombre") String nombre) {
+        return LogicService.getUsuario(nombre);
+    }
+}

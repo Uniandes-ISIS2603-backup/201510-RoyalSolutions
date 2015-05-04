2e077551e900e2ead6afba205e8d17590f89ff7e
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2015-05-03 22:30:47 -0500

    Varios

--- Backend/RoyalVacations.services/src/main/webapp/src/modules/ciudad/ciudad.mod.js
+++ Backend/RoyalVacations.services/src/main/webapp/src/modules/ciudad/ciudad.mod.js
@@ -2,7 +2,7 @@
     var ciudadModule = angular.module('ciudadModule', ['CrudModule', 'MockModule','paisModule']);
 
     ciudadModule.constant('ciudad.context', 'ciudad');
- ciudadModule.constant('ciudad.skipMock', true);
+    ciudadModule.constant('ciudad.skipMock', true);
 
      ciudadModule.config(['ciudad.context', 'MockModule.urlsProvider','ciudad.skipMock', function (context, urlsProvider, skipMock) {
              urlsProvider.registerUrl(context, skipMock);
--- Backend/RoyalVacations.services/src/main/webapp/src/shared/mocks/mock.mod.js
+++ Backend/RoyalVacations.services/src/main/webapp/src/shared/mocks/mock.mod.js
@@ -62,10 +62,10 @@
                     mockUrls(urls[i]);
                 }
             }
-  function skipUrl(entity_url) {
+            function skipUrl(entity_url) {
                  var fullUrl = baseUrl + '/' + entity_url;
                  var url_regexp = new RegExp(fullUrl + '/([0-9]+)');
-                $httpBackend.whenGET(fullUrl).passThrough();
+                 $httpBackend.whenGET(fullUrl).passThrough();
                  $httpBackend.whenGET(url_regexp).passThrough();
                  $httpBackend.whenPOST(fullUrl).passThrough();
                  $httpBackend.whenPUT(url_regexp).passThrough();

d8f28acfea72efaf0927d84b32ccb0c199ebc5da
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2015-05-05 11:06:00 -0500

    Modulo login 3

--- Backend/RoyalVacations.services/src/main/webapp/src/modules/login/login.ctrl.js
+++ Backend/RoyalVacations.services/src/main/webapp/src/modules/login/login.ctrl.js
@@ -5,6 +5,28 @@
             loginService.extendCtrl(this, $scope);
             
             this.fetchRecords();
+       
+            loginService.fetchRecords().then(function(data){
+                $scope.loginRecords = data;
+            });
+            
+            this.validarUsuario = function( ){
+                window.alert("Estamos validando");
+                for(var i in $scope.loginRecords){
+                    if ($scope.loginRecords[i].id === currentRecord.id ) {
+                        if( $scope.loginRecords[i].password === currentRecord.password ) {
+                           $scope.loginUsuarioActual = currentRecord.id; 
+                        }   
+                        else{
+                            window.alert("Lo sentimos,login incorrecto");
+                        }
+                    }
+                    else {
+                        this.saveRecord;
+                        $scope.loginUsuarioActual = currentRecord.id;
+                    }
+                }
+            };
             
         }]);
 })(window.angular);
